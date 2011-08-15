d8f46ff5153d22bdc0c82161daece605693131d9
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2011-08-15 00:32:40 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OtakukingdomPrototype::Application.config.secret_token = '3cd32c1bc68d18c5a981ea48b5e305177f26ba98d67acb3ff947065143395c71b034f0e3fafe888b67080ab8d5567774d29051c9f239841764ec7458c94d421f'
